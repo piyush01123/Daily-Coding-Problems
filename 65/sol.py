02128b3ee4897e249d9101118b3ed6e22fed5c86
piyush-kgp <piyushsinghkgpian@gmail.com>
2019-02-03 21:32:44 +0530

    wooh spiral problem solved

--- 65/sol.py
+++ 65/sol.py
@@ -1,6 +1,20 @@
 
 def printSpiral(A):
     while A:
-        for a in A[0]:
-            print(a)
-        pass
+        top = A.pop(0)
+        bottom = A.pop()
+        for item in top:
+            print(item)
+        for rem in A:
+            print(rem.pop())
+        for item in bottom[::-1]:
+            print(item)
+        for rem in A[::-1]:
+            print(rem.pop(0))
+
+A = [[1,  2,  3,  4,  5],
+ [6,  7,  8,  9,  10],
+ [11, 12, 13, 14, 15],
+ [16, 17, 18, 19, 20]]
+
+printSpiral(A)
