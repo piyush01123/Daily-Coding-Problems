69a344c7b36f9eebcb3227b6b94598041f42e3e5
piyush-kgp <piyushsinghkgpian@gmail.com>
2019-04-04 10:31:27 +0530

    b tree 2 sum

---
+++ 125/sol.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+
+struct node{
+  // b. tree data structure
+  int data;
+  node *left, *right;
+};
+
+
+node *createNode(int data){
+  // create a b. tree node
+  node *temp = new node;
+  temp->data = data;
+  temp->left = temp->right = NULL;
+  return temp;
+}
+
+
+void sumNodes(node *root, int k, unordered_set<int> &comps, vector<int> &v){
+  // recursively find pair
+  if (comps.find(root->data) != comps.end()) v = {root->data, k-root->data};
+  comps.insert(k-root->data);
+  if (root->left != NULL) sumNodes(root->left, k, comps, v);
+  if (root->right != NULL) sumNodes(root->right, k, comps, v);
+}
+
+
+void test(){
+  // crates and runs soln with test case
+  node *root = createNode(10);
+  root->left = createNode(5);
+  root->right = createNode(15);
+  root->right->left = createNode(11);
+  root->right->right = createNode(15);
+  unordered_set<int> comps;
+  vector<int> v;
+  sumNodes(root, 20, comps, v);
+  if (v.size()==0) cout << "Pair not found\n";
+  cout << "K = 20, required nodes = {" << v[0] << ", " << v[1] << "}\n";
+}
+
+
+int main(){
+  // run test
+  test();
+}
