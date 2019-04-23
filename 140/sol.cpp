7b00f7f6b3b37a554363e488a4d585038fc2f95a
piyush-kgp <piyushsinghkgpian@gmail.com>
2019-04-23 14:57:40 +0530

    linear time constant space

--- 140/sol.cpp
+++ 140/sol.cpp
@@ -2,22 +2,48 @@
 #include <iostream>
 #include <unordered_map>
 
+// int *occurOnce(int *arr){
+//   // linear time linear space
+//   int *A = new int[2];
+//   std::unordered_map<int, int> H;
+//   for (int i=0; i<sizeof(arr); i++){
+//     H[arr[i]]++;
+//   }
+//   int k = 0;
+//   for (int i=0; i<sizeof(arr); i++){
+//     if (H[arr[i]]==1){
+//       A[k] = arr[i];
+//       k++;
+//     }
+//   }
+//   return A;
+
+
 int *occurOnce(int *arr){
-  // linear time linear space
-  int *A = new int[2];
-  std::unordered_map<int, int> H;
+  // linear time constant space using bit ops
+  int xored = 0;
   for (int i=0; i<sizeof(arr); i++){
-    H[arr[i]]++;
+    xored = xored^arr[i];
   }
-  int k = 0;
+  // now the question boils down to this - xored is the xor of which 2 items
+  // in the array
+  int right_most_set_bit = xored&(~(xored-1));
+  // this is the first bit from right with different values (one has 0 other has 1)
+  // then we can form two groups based on whether or not their right_most_set_bit is 1 or 0. each group's xor are the answer
+  int x = 0;
+  int y = 0;
   for (int i=0; i<sizeof(arr); i++){
-    if (H[arr[i]]==1){
-      A[k] = arr[i];
-      k++;
+    if ((right_most_set_bit&arr[i])==0){
+      x^=arr[i];
+    }else{
+      y^=arr[i];
     }
   }
+  int *A = new int[2];
+  A[0] = x;
+  A[1] = y;
   return A;
-
+}
 
 void test(){
   int arr[] = {2, 4, 6, 8, 10, 2, 6, 10};
