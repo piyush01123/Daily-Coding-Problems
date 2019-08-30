2c249d4b493f6bcd6bed189a7e75ab287f06eb2e
piyush-kgp <piyushsinghkgpian@gmail.com>
2019-08-31 01:33:01 +0530

    removed temp

--- temp/ImgCaption/download_data.sh
+++
@@ -1,6 +0,0 @@
-
-wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
-unzip annotations_trainval2014.zip
-
-wget http://images.cocodataset.org/zips/train2014.zip
-unzip train2014.zip
--- temp/nand2tetris/Xor.tst
+++
@@ -1,7 +0,0 @@
-load Xor.hdl;
-output-file Xor.out;
-output-list a b out;
-set a 0, set b 0, eval, output;
-set a 1, set b 0, eval, output;
-set a 0, set b 1, eval, output;
-set a 1, set b 1, eval, output;
